d246b63f5ff54f70384e430c9a3897400fe9ff36
Laura Rinaldi <rinaldilaurinha@gmail.com>
2023-07-25 18:06:42 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import ContactsList from "./components/ContactsList";
+import ContactsCard from "./components/ContactsCard";
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <ContactsList />
     </div>
   );
 }
